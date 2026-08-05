03fe8d07247c6fa1b38947d5f721e67b88fc42d3
phihash <insharp0220@gmail.com>
2026-08-05 23:29:30 +0900

    スキーマ

---
+++ backend/app/schemas/user.py
@@ -0,0 +1,6 @@
+from pydantic import BaseModel, ConfigDict
+
+class UserOut(BaseModel):
+    model_config = ConfigDict(from_attributes=True)  #SQLAlchemyモデルからPydanticモデルの変換許可
+    id: int
+    login_id: str
