c266db2acedc7ea1c43512d39728811594bebb9b
phihash <insharp0220@gmail.com>
2026-08-06 01:02:04 +0900

    mainファイル

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
 
+from app import models
+from app.database import Base, engine
+from app.routers import auth
+from app.seed import seed_user
+ 
+Base.metadata.create_all(bind=engine)
+seed_user()
+
 app = FastAPI(title="hanasu API")
+
+app.include_router(auth.router)
 
 @app.get("/")
 def root():
