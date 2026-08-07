41677647db156a700e6f7059ea2dd0d668c940fd
Kyoya Takumu <kyoyatakumu@gmail.com>
2026-08-07 20:18:38 +0900

    fix: container_imageのハードコードを修正

--- infra/dev/locals.tf
+++ infra/dev/locals.tf
@@ -1,5 +1,5 @@
 locals {
   name_prefix     = "hanasu"
   container_port  = 8000
-  container_image = "926046660554.dkr.ecr.ap-northeast-1.amazonaws.com/hanasu-api:latest"
+  container_image = "${aws_ecr_repository.api.repository_url}:latest"
 }
