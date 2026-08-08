417989c1ea1a610d15fff1fca01b6e4c2c1b1e64
Kyoya Takumu <kyoyatakumu@gmail.com>
2026-08-08 22:37:26 +0900

    fix: alb,publicサブネット,dockerfileのセキュリティー面の修正

--- infra/dev/alb.tf
+++ infra/dev/alb.tf
@@ -1,9 +1,10 @@
 resource "aws_lb" "main" {
-  name               = "${local.name_prefix}-api"
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = [aws_security_group.alb.id]
-  subnets            = aws_subnet.public[*].id
+  name                       = "${local.name_prefix}-api"
+  internal                   = false
+  load_balancer_type         = "application"
+  security_groups            = [aws_security_group.alb.id]
+  subnets                    = aws_subnet.public[*].id
+  drop_invalid_header_fields = true
 
   tags = {
     Name = "${local.name_prefix}-api"
--- infra/dev/network.tf
+++ infra/dev/network.tf
@@ -26,7 +26,7 @@ resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.${count.index + 1}.0/24"
   availability_zone       = data.aws_availability_zones.available.names[count.index]
-  map_public_ip_on_launch = true
+  map_public_ip_on_launch = false
 
   tags = {
     Name = "${local.name_prefix}-public-${count.index + 1}"
